e160ddcf680be67de0f5331c3714a54682669277
Bryan Rojas <bryanrojascs@gmail.com>
2022-02-09 11:32:01 -0800

    Updated resume link

--- src/components/ResumeButton.js
+++ src/components/ResumeButton.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import resume from '../static/resume/resume.pdf'
+import Resume from '../static/resume/resume_bryan_rojas.pdf'
 
 class Buttons extends Component {
     render() {
         return(
             <>
                 <div className="row">
-                    <button type="button" class="btn btn-primary"><a href={resume} style={{color: 'white'}}>Resume</a></button>
+                    <button type="button" class="btn btn-primary"><a href={Resume} target = "_blank" style={{color: 'white'}}>Resume</a></button>
                     &nbsp;
                     <button type="button" class="btn btn-primary"><a href="https://www.linkedin.com/in/~bryan/" style={{color: 'white'}}>LinkedIn</a></button>
                     &nbsp;
