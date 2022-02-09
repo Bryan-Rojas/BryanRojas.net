28206c9301a5fc4fcc89425e60e56c74a20ca550
Bryan Rojas <bryanrojascs@gmail.com>
2022-02-09 11:43:04 -0800

    Resume updated, again

--- src/components/ResumeButton.js
+++ src/components/ResumeButton.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-import Resume from '../static/resume/resume_bryan_rojas.pdf'
 
 class Buttons extends Component {
     render() {
         return(
             <>
                 <div className="row">
-                    <button type="button" class="btn btn-primary"><a href={Resume} target = "_blank" style={{color: 'white'}}>Resume</a></button>
+                    <button type="button" class="btn btn-primary"><a href="https://res.cloudinary.com/dzwtspghi/image/upload/v1644435651/Bryan_Rojas_-_Resume_-_2022_wdieln.pdf" target = "_blank" style={{color: 'white'}}>Resume</a></button>
                     &nbsp;
                     <button type="button" class="btn btn-primary"><a href="https://www.linkedin.com/in/~bryan/" style={{color: 'white'}}>LinkedIn</a></button>
                     &nbsp;
