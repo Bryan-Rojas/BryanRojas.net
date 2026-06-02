5e004e2ff0afca99794762b69f1bf8270dff7a7d
Bryan Rojas <bryan@bryanrojas.net>
2026-06-02 21:01:43 +0000

    chore: add build artifacts for deployment

---
+++ build/precache-manifest.8b486b5f2ac9246c8e28c939fecc017b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef6e3d33705b9a8bd9aa8234c34edfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "489521079d95e0bbaec8",
+    "url": "/static/css/main.47c0d2ec.chunk.css"
+  },
+  {
+    "revision": "ea7bb60fb9f6c63030df",
+    "url": "/static/js/2.f383b3a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f383b3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489521079d95e0bbaec8",
+    "url": "/static/js/main.d61ce475.chunk.js"
+  },
+  {
+    "revision": "78874e34c80c772b24fc",
+    "url": "/static/js/runtime-main.323b35e3.js"
+  },
+  {
+    "revision": "b525b7fddfb1e570d4bf787a64aea56d",
+    "url": "/static/media/selfie.b525b7fd.png"
+  }
+]);
